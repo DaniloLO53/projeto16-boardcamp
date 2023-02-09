f0af119d3ed5588c5b27f8c5a40b4fbcd43b9aa6
Danilo Leão <olin_ad@hotmail.com>
2023-02-09 15:05:10 -0300

    Feature - Return Rent: controllers, routes

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -49,7 +49,51 @@ async function insertRental(request, response, next) {
   }
 };
 
+async function returnRent(request, response, next) {
+  const { id } = request.params;
+
+  try {
+    const returnDateRaw = Date.now();
+    const returnDate = dayjs(returnDateRaw).format('YYYY-MM-DD');
+    const result = await db.query('SELECT * FROM rentals WHERE "id" = $1', [id]);
+    const rent = result.rows[0];
+    const { rentDate, daysRented, originalPrice, customerId, gameId } = rent;
+
+    const delayDays = (returnDateRaw - rentDate) / 60 / 60 / 24 / 1000;
+    const delayDaysToPay = Math.floor(delayDays) - daysRented;
+
+    const delayFee = delayDaysToPay > 0
+      ? delayDaysToPay * (originalPrice / daysRented)
+      : 0;
+
+    if (rent && !rent.returnDate) {
+      await db.query(`UPDATE rentals SET
+      "customerId" = $1,
+      "gameId" = $2,
+      "daysRented" = $3,
+      "rentDate" = $4,
+      "returnDate" = $5,
+      "originalPrice" = $6,
+      "delayFee" = $7
+      WHERE "id" = $8`,
+        [customerId, gameId, daysRented, rentDate, returnDate, originalPrice, delayFee, id]);
+
+      return response.sendStatus(200);
+    }
+
+    if (rent.returnDate) return response.send(400);
+    return response.send(404);
+
+
+  } catch (error) {
+    console.log('Error: ', error);
+
+    return response.sendStatus(500);
+  }
+};
+
 export {
   getRentals,
   insertRental,
+  returnRent,
 };
--- src/routers/rentals/rentals.routes.js
+++ src/routers/rentals/rentals.routes.js
@@ -2,12 +2,14 @@ import express from 'express';
 import {
   getRentals,
   insertRental,
+  returnRent,
 } from '../../controllers/rentals.controllers.js';
 import rentalsValidator from '../../middlewares/rentalsValidator.middleware.js';
 
 const rentalsRoute = express.Router();
 
 rentalsRoute.post('/rentals', rentalsValidator, insertRental);
+rentalsRoute.post('/rentals/:id/return', returnRent);
 rentalsRoute.get('/rentals', getRentals);
 
 export default rentalsRoute;
